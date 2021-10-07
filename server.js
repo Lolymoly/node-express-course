9a26da360884d0b663d8803dad23031770118356
Lolymoly <teetat.wis@gmail.com>
2021-10-07 23:15:03 +0700

    add login POST route

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
 ]
 app.get('/users', function(req,res){
+    // if come in /users/, it will show mockUserData
     res.json({
         success: true,
         message: 'successfully got users. Nice!',
@@ -14,12 +17,34 @@ app.get('/users', function(req,res){
 })
 
 app.get('/users/:id',function(req,res){
+    // if come in /users/id/ , it will show id
     console.log(req.params.id)
     res.json({
         success: true,
         message: 'got one user',
         user: req.params.id
     })
+})
+
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
 })
 
 app.listen(8000, function(){
